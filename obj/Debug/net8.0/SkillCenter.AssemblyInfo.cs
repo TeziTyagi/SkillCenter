1086b506c26ca22ad24fa56761722cad0326ffe9
Tezi <Tezityagi008@gmail.com>
2024-09-13 13:07:45 +0530

    done

--- obj/Debug/net8.0/SkillCenter.AssemblyInfo.cs
+++ obj/Debug/net8.0/SkillCenter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac78912c85e49aab5fc5305cab4417684e4b9b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkillCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
